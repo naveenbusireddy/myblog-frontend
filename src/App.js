484714bec4ad70fb1f33ebcf58d209972ff73c9a
Naveen Busireddy <naveenbusireddy@gmail.com>
2022-10-21 22:37:50 +0530

    Added Components and Pages folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Article from "./pages/Article";
+import ArticlesList from "./pages/ArticlesList";
+import NotFound from "./pages/NotFound";
+
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
+    <BrowserRouter>
+      <NavBar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticlesList />} />
+          <Route path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
