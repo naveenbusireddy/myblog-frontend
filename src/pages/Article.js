619f24f4d1587c692023a6f13ec479d4b9112818
Naveen Busireddy <naveenbusireddy@gmail.com>
2022-10-26 22:14:28 +0530

    Blog Project frontend final

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,41 +1,43 @@
 import "whatwg-fetch";
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import Articles from "../components/Articles";
 import articles from "./article-content";
+import Articles from "../components/Articles";
+
 
 const Article = () => {
-  const name = useParams();
-  const article = articles.find((article) => article.name === name);
+    const name = useParams();
+    const article = articles.find((article) => article.name === name.name);
+    
+    const [articleInfo, setArticleInfo] = useState({ comments: [] });
 
-  const [articleInfo, setArticleInfo] = useState({ comments: [] });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(`/api/articles/${name}`);
-      const body = await result.json();
-      setArticleInfo(body);
-    };
-    fetchData();
-  }, [name.name]);
-
-  if (!article) return <h1>Article does not exist!</h1>;
-  const otherArticles = articles.filter(
-    (article) => article.name !== name.name
-  );
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name.name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name.name]);
+    
+    
+    if(!article) return <h1>Article does not exist</h1>;
+    const otherArticles = articles.filter((article) => article.name !== name.name);
 
   return (
     <div className="mb-20">
-      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-        {article.title}
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
+      {article.title}
       </h1>
-      {article.content.map((paragraph, index) => (
-        <p className="mb-4" key={index}>
-          {paragraph}
+      {article.content.map((paragraph, index) =>
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
+            {paragraph}
         </p>
-      ))}
+      )}
 
-      <h1 className="sm:text-2x text-xl font-bold mt-6 mb-6 text-gray-900">
+
+      <h1 className="sm:text-2x text-xl font-bold mt-4 mb-4 text-gray-900">
         Other Articles:
       </h1>
       <div className="flex flex-wrap -m-4">
--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -5,10 +5,9 @@ import Articles from "../components/Articles";
 const ArticlesList = () => {
   return (
     <>
-      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-        Articles List
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
+        Article List
       </h1>
-
       <div className="container py-4 mx-auto">
         <div className="flex flex-wrap -m-4">
           <Articles articles={articles} />
